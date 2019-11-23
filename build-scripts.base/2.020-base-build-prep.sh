af41473da77325d9d1655ae92967e240053cf781
Travis Davies <tdavies@darkphysics.net>
2019-11-23 13:30:46 -0800

    Fix mount conditionals for build-scripts.base/2.020-base-build-prep.sh

--- build-scripts.base/2.020-base-build-prep.sh
+++ build-scripts.base/2.020-base-build-prep.sh
@@ -6,44 +6,50 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 source $SCRIPTPATH/../include.sh
 
 mkdir -pv $BUILDROOT/{dev,proc,sys,run}
-check $? "2.020-base-build-prep.sh mkdir -pv $BUILDROOT/{dev,proc,sys,run}"
+check $? "6.02-prep mkdir -pv $BUILDROOT/{dev,proc,sys,run}"
 
 if [ ! -e $BUILDROOT/dev/console ] ; then
 	mknod -m 600 $BUILDROOT/dev/console c 5 1
-	check $? "2.020-base-build-prep.sh mknod -m 600 $BUILDROOT/dev/console c 5 1"
+	check $? "2.020-base-build-prep: mknod -m 600 $BUILDROOT/dev/console c 5 1"
 fi
 
 if [ ! -e $BUILDROOT/dev/null ] ; then
 	mknod -m 666 $BUILDROOT/dev/null c 1 3
-	check $? "2.020-base-build-prep.sh mknod -m 666 $BUILDROOT/dev/null c 1 3"
+	check $? "2.020-base-build-prep: mknod -m 666 $BUILDROOT/dev/null c 1 3"
 fi
 
-if [ ! -e $BUILDROOT/dev ] ; then
+mount | grep "$BUILDROOT/dev"
+if [ $? -ne 0 ] ; then
 	mount -v --bind /dev $BUILDROOT/dev
-	check $? "2.020-base-build-prep.sh mount -v --bind /dev $BUILDROOT/dev"
+	check $? "2.020-base-build-prep: mount -v --bind /dev $BUILDROOT/dev"
 fi
 
-if [ ! -e $BUILDROOT/dev/pts ] ; then
+mount | grep "$BUILDROOT/dev/pts"
+if [ $? -ne 0 ] ; then
 	mount -vt devpts devpts $BUILDROOT/dev/pts -o gid=5,mode=620
-	check $? "2.020-base-build-prep.sh mount -vt devpts devpts $BUILDROOT/dev/pts"
+	check $? "2.020-base-build-prep: mount -vt devpts devpts $BUILDROOT/dev/pts"
 fi
 
-if [ ! -e $BUILDROOT/proc ] ; then
+mount | grep "$BUILDROOT/proc"
+if [ $? -ne 0 ] ; then
 	mount -vt proc proc $BUILDROOT/proc
-	check $? "2.020-base-build-prep.sh mount -vt proc proc $BUILDROOT/proc"
+	check $? "2.020-base-build-prep: mount -vt proc proc $BUILDROOT/proc"
 fi
 
-if [ ! -e $BUILDROOT/sys ] ; then
+mount | grep "$BUILDROOT/sys"
+if [ $? -ne 0 ] ; then
 	mount -vt sysfs sysfs $BUILDROOT/sys
-	check $? "2.020-base-build-prep.sh mount -vt sysfs sysfs $BUILDROOT/sys"
+	check $? "2.020-base-build-prep: mount -vt sysfs sysfs $BUILDROOT/sys"
 fi
 
-if [ ! -e $BUILDROOT/run ] ; then
+mount | grep "$BUILDROOT/run"
+if [ $? -ne 0 ] ; then
 	mount -vt tmpfs tmpfs $BUILDROOT/run
-	check $? "2.020-base-build-prep.sh mount -vt tmpfs tmpfs $BUILDROOT/run"
+	check $? "2.020-base-build-prep: mount -vt tmpfs tmpfs $BUILDROOT/run"
 fi
 
 if [ -h $BUILDROOT/dev/shm ]; then
   mkdir -pv $BUILDROOT/$(readlink $BUILDROOT/dev/shm)
+  check $? "2.020-base-build-prep: mkdir -pv $BUILDROOT/$(readlink $BUILDROOT/dev/shm)"
 fi
 
