26301637d1b96734c4fed2b64f50547b45d7933a
Travis Davies <tdavies@darkphysics.net>
2019-12-03 09:04:39 -0800

    Fix incorrect arguments to report() in 3.080-grub.sh
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts.config/3.080-grub.sh
+++ build-scripts.config/3.080-grub.sh
@@ -12,16 +12,16 @@ ROOT_PART='\/dev\/'$ROOT_PART
 
 LOOP=0
 while [ $LOOP -eq 0 ] ; do
-    report -e "\n\nTime to install Grub." 
+    report "\n\nTime to install Grub." 
     report "build.conf specifys:" 
     report "- ROOT_DEVICE: /dev/$ROOT_DEV"
     report "- ROOT_PARTITION: $ROOT_PARTITION"
     report "- GRUB sees ROOT_DEVICE as(hdX,Y): $SET_ROOT "
     report "NOTE: Grub see the root device number (hdX) as 1 less than ROOT_DEVICE number."
     report "the partition number (Y) is the same as ROOT_PARTITION's."
-    report -e "\n"
+    report "\n"
     report "If you DO NOT wish to install grub to this device, type 'no'" 
-    report -n "Install grub to /dev/$ROOT_DEV? (yes/no): "
+    report "Install grub to /dev/$ROOT_DEV? (yes/no): "
     read CHOICE 
 
     if [ "$CHOICE" == "yes" ] || [ "$CHOICE" == "no" ] ; then
