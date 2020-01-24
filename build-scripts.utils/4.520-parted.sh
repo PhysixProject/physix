40ea14b2b0f4ba7e03cc20b5dc1cc21258e7b357
Travis Davies <tdavies@darkphysics.net>
2020-01-24 13:08:57 -0800

    Add parted build script to utilities
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

---
+++ build-scripts.utils/4.520-parted.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source /physix/include.sh || exit 1
+cd $SOURCE_DIR/$1 || exit 1
+
+su physix -c 'sed -i '/utsname.h/a#include <sys/sysmacros.h>' libparted/arch/linux.c '
+chroot_check $? "sed libparted/arch/linux.c"
+
+su physix -c './configure --prefix=/usr --disable-static '
+chroot_check $? "configure"
+
+su physix -c 'make '
+chroot_check $? "make"
+
+make -C doc html                                       &&
+makeinfo --html      -o doc/html       doc/parted.texi &&
+makeinfo --plaintext -o doc/parted.txt doc/parted.texi
+chroot_check $? "mutt : make install"
+
