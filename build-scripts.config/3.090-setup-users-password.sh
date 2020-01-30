895d82cdd4e23302237a7263974c12705491c246
Travis Davies <tdavies@darkphysics.net>
2020-01-30 15:52:15 -0800

    - Add bash profile - cp bash profile to users home dirs in 3.090-setup-users-password.sh - Remove prompt to set physix passwd, as physix is not a normal user
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts.config/3.090-setup-users-password.sh
+++ build-scripts.config/3.090-setup-users-password.sh
@@ -11,13 +11,22 @@ while [ $LOOP -eq 0 ] ; do
 	passwd root
 	if [ $? -eq 0 ] ; then LOOP=1; fi
 done
+
+cp /physix/build-scripts.config/configs/profile /root/.profile
+chroot_check $? "Create /root/.profile"
+
 cp /physix/build-scripts.config/configs/etc_bashrc /root/.bashrc
-chroot_check $? "Create root/.bashrc"
+chroot_check $? "Create /root/.bashrc"
 
 if [ $CONF_GEN_USER ] ; then
 	echo "build.conf specifys a general user to be created: $CONF_GEN_USER"
 	useradd -m $CONF_GEN_USER
 	chroot_check $? "useradd -m $CONF_GEN_USER"
+
+	cp /physix/build-scripts.config/configs/profile /root/.profile
+	chroot_check $? "Create ~/.profile"
+	chown $CONF_GEN_USER:$CONF_GEN_USER /home/$CONF_GEN_USER/.profile
+	chroot_check $? "chown /home/$CONF_GEN_USER/.profile"
 
 	cp /physix/build-scripts.config/configs/etc_bashrc /home/$CONF_GEN_USER/.bashrc
 	chroot_check $? "Create /home/$CONF_GEN_USER/.bashrc"
@@ -33,9 +42,4 @@ fi
 
 useradd -m physix
 chroot_check $? "useradd  physix"
-LOOP=0
-while [ $LOOP -eq 0 ] ; do
-        passwd physix
-        if [ $? -eq 0 ] ; then LOOP=1; fi
-done
 
