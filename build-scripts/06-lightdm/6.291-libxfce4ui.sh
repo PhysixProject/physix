ae2bba33936097e4efad30f68fdab223c9746c3b
Travis Davies <tdavies@darkphysics.net>
2020-03-26 15:25:17 -0700

    Adding -j$NPROC to make command for the following scripts  modified:   build-scripts/06-lightdm/6.290-libxfce4util.sh  modified:   build-scripts/06-lightdm/6.291-libxfce4ui.sh  modified:   build-scripts/06-lightdm/6.292-libxfconf.sh
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts/06-lightdm/6.291-libxfce4ui.sh
+++ build-scripts/06-lightdm/6.291-libxfce4ui.sh
@@ -5,7 +5,7 @@ cd $SOURCE_DIR/$1 || exit 1
 su physix -c './configure --prefix=/usr --sysconfdir=/etc'
 chroot_check $? "configure"
 
-su physix -c 'make'
+su physix -c "make -j$NPROC"
 chroot_check $? "make"
 
 make install
