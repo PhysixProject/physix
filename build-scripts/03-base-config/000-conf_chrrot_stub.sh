f69e244bfc2065b37ce93ce99179780f8c322b13
Travis Davies <tdavies@darkphysics.net>
2020-03-28 09:04:11 -0700

    Fix 03-base-config/000-conf_chrrot_stub.sh and scripts/chroot-link-base.sh to link against /usr/bin/... instead of /tools/bin/...
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts/03-base-config/000-conf_chrrot_stub.sh
+++ build-scripts/03-base-config/000-conf_chrrot_stub.sh
@@ -6,10 +6,8 @@ source /mnt/physix/opt/physix/include.sh || exit 1
 SCRIPT=$1
 SOURCES=${2:-''}
 
-chroot "$BUILDROOT" /tools/bin/env -i HOME=/root  TERM="$TERM" \
-        PS1='(physix chroot) \u:\w\$ '                         \
-        PATH=/bin:/usr/bin:/sbin:/usr/sbin                     \
+chroot "$BUILDROOT" /usr/bin/env -i HOME=/root  TERM="$TERM" \
+        PS1='(physix chroot) \u:\w\$ '                       \
+        PATH=/bin:/usr/bin:/sbin:/usr/sbin                   \
         /bin/bash --login -c "/opt/physix/build-scripts/03-base-config/$SCRIPT $SOURCES"
-
-
 
