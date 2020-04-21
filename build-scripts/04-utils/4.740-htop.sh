0462f6093aa9a1190d8628f9d75248e4c975b233
Travis Davies <tdavies@darkphysics.net>
2020-04-21 12:36:07 -0700

    Fix 4.740-htop.sh:  Add missing ./configure call
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts/04-utils/4.740-htop.sh
+++ build-scripts/04-utils/4.740-htop.sh
@@ -2,7 +2,7 @@
 source /opt/physix/include.sh || exit 1
 cd $SOURCE_DIR/$1 || exit 1
 
-su physix -c './autogen.sh'
+su physix -c './autogen.sh && ./configure'
 chroot_check $? "configure"
 
 su physix -c "make -j$NPROC"
