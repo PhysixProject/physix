cf2fc4f45262ecab94b645bc5c00c2db0ddeda2f
Travis Davies <tdavies@darkphysics.net>
2020-05-03 11:41:24 -0700

    06-lightdm.json: Add libcanberra
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

---
+++ build-scripts/06-lightdm/6.320-libcanberra.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /opt/physix/include.sh || exit 1
+cd $SOURCE_DIR/$1 || exit 1
+
+
+su physix -c './configure --prefix=/usr --disable-oss'
+chroot_check $? "configure"
+
+su physix -c 'make'
+chroot_check $? "make"
+
+make docdir=/usr/share/doc/libcanberra-0.30 install
+chroot_check $? "make install"
+
