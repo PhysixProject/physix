bfbd05392ac9ef5365c1d9e9876eeb3527923478
Travis Davies <tdavies@darkphysics.net>
2020-05-09 11:27:16 -0700

    Add Thunderbird-68.5.0 to 08-xapps recipe
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

---
+++ build-scripts/08-xapps/8.150-thunderbird.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /opt/physix/include.sh || exit 1
+cd $SOURCE_DIR/$1 || exit 1
+
+su physix -c 'cp /opt/physix/build-scripts/08-xapps/configs/thunderbird/mozconfig .'
+chroot_check $? 'Write config'
+
+su physix -c 'source /etc/profile.d/rustc.sh && ./mach build'
+chroot_check $? 'mach build'
+
+./mach install
+chroot_check $? 'mach install'
+
