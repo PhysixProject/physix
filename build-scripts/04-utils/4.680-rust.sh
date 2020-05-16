92f0f9b27ab98a313381ddff19ba719b804d2f4e
Travis Davies <tdavies@darkphysics.net>
2020-05-16 09:09:05 -0700

    4.680-rust.sh: In the event that rust is being re-built, remove link                before removing source of link to prevent interactive                prompt.
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts/04-utils/4.680-rust.sh
+++ build-scripts/04-utils/4.680-rust.sh
@@ -7,12 +7,12 @@ su physix -c 'ping -q -c 3  8.8.8.8'
 chroot_check $? "Ping: Internet Reachable"
 # If this fails, try: 'ip route add default via <your-router-ip-addr>'
 
-if [ -e /usr/bin/rustc-1.35.0 ] ; then
-	rm -r /usr/bin/rustc-1.35.0
+if [ -e /usr/bin/rustc ] ; then
+        rm /usr/bin/rustc
 fi
 
-if [ -e /usr/bin/rustc ] ; then
-	rm /usr/bin/rustc
+if [ -e /usr/bin/rustc-1.35.0 ] ; then
+	rm -r /usr/bin/rustc-1.35.0
 fi
 
 mkdir /usr/bin/rustc-1.35.0  &&
