6751ae320136df58ab93abb64c37b01bd3faeba0
Travis Davies <tdavies@darkphysics.net>
2020-05-19 14:39:53 -0700

    Update to gtk 3.24.20 modified:   06-lightdm.json modified:   build-scripts/06-lightdm/6.280-gtk.sh
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts/06-lightdm/6.280-gtk.sh
+++ build-scripts/06-lightdm/6.280-gtk.sh
@@ -2,23 +2,40 @@
 source /opt/physix/include.sh || exit 1
 cd $SOURCE_DIR/$1 || exit 1
 
-cd ./build
-su physix -c "meson --prefix=/usr     \
-              -Dcolord=yes      \
-              -Dgtk_doc=false   \
-              -Dman=false        \
-              -Dbroadway_backend=true .."
+
+su physix -c 'source ~/.profile && env && ./configure --prefix=/usr              \
+            --sysconfdir=/etc          \
+            --enable-broadway-backend  \
+            --enable-x11-backend       \
+            --enable-wayland-backend  --libdir=/usr/lib64'
 chroot_check $? "configure"
 
-#su physix -c "sed -i 's/'--nonet'//' build.ninja"
-#chroot_check $? "Remove --nonet"
+su physix -c 'make'
+chroot_check $? "make"
 
-su physix -c 'ninja'
-chroot_check $? "ninja"
+make install
+chroot_check $? "make install"
 
-ninja install
-chroot_check $? "ninja install"
 
-#su physix -c 'mkdir -vp ~/.config/'
-#cp -rv /physix/build-scripts/lightdm/configs/gtk-3.0/settings.ini 
+mkdir -vp ~/.config/gtk-3.0
+cat > ~/.config/gtk-3.0/settings.ini << "EOF"
+[Settings]
+gtk-theme-name = Adwaita
+gtk-icon-theme-name = oxygen
+gtk-font-name = DejaVu Sans 12
+gtk-cursor-theme-size = 18
+gtk-toolbar-style = GTK_TOOLBAR_BOTH_HORIZ
+gtk-xft-antialias = 1
+gtk-xft-hinting = 1
+gtk-xft-hintstyle = hintslight
+gtk-xft-rgba = rgb
+gtk-cursor-theme-name = Adwaita
+EOF
+
+cat > ~/.config/gtk-3.0/gtk.css << "EOF"
+*  {
+   -GtkScrollbar-has-backward-stepper: 1;
+   -GtkScrollbar-has-forward-stepper: 1;
+}
+EOF
 
