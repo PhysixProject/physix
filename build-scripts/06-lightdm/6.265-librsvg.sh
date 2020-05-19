c5b342c513a6f71a750f26f8922b3b3c32fcade4
Travis Davies <tdavies@darkphysics.net>
2020-05-19 13:05:51 -0700

    6.265-librsvg.sh: Add --libdir=/usr/lib64 to configure
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts/06-lightdm/6.265-librsvg.sh
+++ build-scripts/06-lightdm/6.265-librsvg.sh
@@ -2,7 +2,7 @@
 source /opt/physix/include.sh || exit 1
 cd $SOURCE_DIR/$1 || exit 1
 
-su physix -c '. /etc/profile && ./configure --prefix=/usr --enable-vala --disable-static'
+su physix -c 'source ~/.profile && ./configure --prefix=/usr --enable-vala --disable-static --libdir=/usr/lib64'
 chroot_check $? 'configure'
 
 su physix -c ". /etc/profile && make -j$NPROC"
