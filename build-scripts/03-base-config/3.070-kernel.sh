06c148efcfe918abf3c8c2b2b8f67575fc26fe70
Tree Davies <tdavies@darkphysics.net>
2020-05-20 15:40:27 -0700

    Update Default Installation Kernel Linux 5.4.41 Update Kernel build script to be more generic for easier maintenance. Update lvm-grub.cfg config for 5.4.41 Kernel
    Signed-off-by: Travis Davies <tdavies@darkphysics.net>

--- build-scripts/03-base-config/3.070-kernel.sh
+++ build-scripts/03-base-config/3.070-kernel.sh
@@ -4,11 +4,13 @@
 source /opt/physix/include.sh || exit 1
 cd $SOURCE_DIR/$1 || exit 1
 
+VERSION='5.4.41'
+
 make mrproper
 chroot_check $? "system config: kernel : make mr_proper"
 
-install --verbose --mode 644 --owner root --group root /opt/physix/build-scripts/03-base-config/configs/linux_config-5.2.8  /opt/sources.physix/BUILDBOX/$1/.config
-chroot_check $? "Set Physix 5.2.8 Linux kernel config"
+install --verbose --mode 644 --owner root --group root /opt/physix/build-scripts/03-base-config/configs/linux_config-$VERSION  /opt/sources.physix/BUILDBOX/$1/.config
+chroot_check $? "Set Linux kernel config"
 
 make -j$NPROC
 chroot_check $? "system config: kernel : make"
@@ -16,20 +18,20 @@ chroot_check $? "system config: kernel : make"
 make modules_install
 chroot_check $? "system config: kernel : make module_install"
 
-install --verbose --mode 644 --owner root --group root arch/x86/boot/bzImage  /boot/vmlinuz-5.2.8.physix.x86_64
-chroot_check $? "/boot/vmlinuz-5.2.8.physix.x86_64"
+install --verbose --mode 644 --owner root --group root arch/x86/boot/bzImage  /boot/vmlinuz-$VERSION.physix.x86_64
+chroot_check $? "/boot/vmlinuz-$VERSION.physix.x86_64"
 
-install --verbose --mode 644 --owner root --group root System.map  /boot/System.map-5.2.8
-chroot_check $? "/boot/System.map-5.2.8"
+install --verbose --mode 644 --owner root --group root System.map  /boot/System.map-$VERSION
+chroot_check $? "/boot/System.map-$VERSION"
 
-install --verbose --mode 644 --owner root --group root .config  /boot/config-5.2.8.physix.x86_64
-chroot_check $? "/boot/config-5.2.8.physix.x86_64"
+install --verbose --mode 644 --owner root --group root .config  /boot/config-$VERSION.physix.x86_64
+chroot_check $? "/boot/config-$VERSION.physix.x86_64"
 
-install -d /usr/share/doc/linux-5.2.8
+install -d /usr/share/doc/linux-$VERSION
 chroot_check $? "system config: kernel : install kernel doc"
 
-cp -rp Documentation/*  /usr/share/doc/linux-5.2.8
-chroot_check $? "install Documentation/* /usr/share/doc/linux-5.2.8"
+cp -rp Documentation/*  /usr/share/doc/linux-$VERSION
+chroot_check $? "install Documentation/* /usr/share/doc/linux-$VERSION"
 
 install --verbose --mode 755 --directory /etc/modprobe.d
 chroot_check $? "Create /etc/modprobe.d"
@@ -37,7 +39,7 @@ chroot_check $? "Create /etc/modprobe.d"
 install --verbose --mode 644 --owner root --group root /opt/physix/build-scripts/03-base-config/configs/usb.conf  /etc/modprobe.d/usb.conf
 chroot_check $? "Install /etc/modprobe.d/usb.conf"
 
-mkinitrd --force /boot/initrd-5.2.8.physix.x86_64 5.2.8
+mkinitrd --force /boot/initrd-$VERSION.physix.x86_64 $VERSION
 chroot_check $? "Install mkinitrd"
 
 install --verbose --mode 744 --owner root --group root /opt/physix/scripts/kinstall  /usr/local/bin
