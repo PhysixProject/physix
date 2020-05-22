3e338f70975433f732df90865b3883ff2270d682
Tree Davies <tdavies@darkphysics.net>
2020-05-22 16:47:00 -0700

    - 6.280-gtk.sh: Add ldconfig call pre and post install so needed               libraries are found.
    - Add 7.025-gdkpix-buf.sh which rebuilds gdkpix-buf. This seems to
      fix the following build error of gtk-2.24.32 built by 7.030-gtk2.sh:
    /usr/bin/gdk-pixbuf-csource --raw --build-list  \
            apple_red  ./apple-red.png              \
                    gnome_foot ./gnome-foot.png     \
            > test-inline-pixbufs.h                 \
    || (rm -f test-inline-pixbufs.h && false)
    failed to load "./apple-red.png": Couldn’t recognize the image file format for file “./apple-red.png”
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- build-scripts/06-lightdm/6.280-gtk.sh
+++ build-scripts/06-lightdm/6.280-gtk.sh
@@ -2,7 +2,7 @@
 source /opt/physix/include.sh || exit 1
 cd $SOURCE_DIR/$1 || exit 1
 
-
+ldconfig
 su physix -c 'source ~/.profile && env && ./configure --prefix=/usr              \
             --sysconfdir=/etc          \
             --enable-broadway-backend  \
@@ -15,12 +15,6 @@ chroot_check $? "make"
 
 make install
 chroot_check $? "make install"
-
-gtk-query-immodules-3.0 --update-cache
-chroot_check $? "gtk-query-immodules-3.0"
-
-glib-compile-schemas /usr/share/glib-2.0/schemas
-chroot_check $? "glib-compile-schemas"
 
 mkdir -vp ~/.config/gtk-3.0
 cat > ~/.config/gtk-3.0/settings.ini << "EOF"
@@ -43,4 +37,13 @@ cat > ~/.config/gtk-3.0/gtk.css << "EOF"
    -GtkScrollbar-has-forward-stepper: 1;
 }
 EOF
+
+ldconfig
+chroot_check $? "ldconfig"
+
+gtk-query-immodules-3.0 --update-cache
+chroot_check $? "gtk-query-immodules-3.0"
+
+glib-compile-schemas /usr/share/glib-2.0/schemas
+chroot_check $? "glib-compile-schemas"
 
