1ff1988b9ea3cca7fec49219743cde8ca3fc4e67
Tree Davies <tdavies@darkphysics.net>
2020-05-26 12:09:18 -0700

    Add 5.4.41 kernel headers to toolchain and base recipes
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- build-scripts/02-base/2.020-linux-api-headers.sh
+++ build-scripts/02-base/2.020-linux-api-headers.sh
@@ -5,16 +5,14 @@ source /opt/physix/include.sh || exit 1
 export LIBRARY_PATH=/usr/lib/:$LIBRARY_PATH
 cd $SOURCE_DIR/$1 || exit 1
 
-
 make mrproper
 chroot_check $? "system-build : linux-api-headers: make mrproper "
 
-make INSTALL_HDR_PATH=dest headers_install
+make headers
 chroot_check $? "system-build : make INSTALL_HDR_PATH=dest headers_install"
 
-find dest/include \( -name .install -o -name ..install.cmd \) -delete
-chroot_check $? "system-build : find dest/include \( -name .install -o -name ..install.cmd \) -delete"
-
-cp -rv dest/include/* /usr/include
+find usr/include -name '.*' -delete &&
+rm usr/include/Makefile &&
+cp -rv usr/include/* /usr/include
 chroot_check $? "system-build : cp -rv dest/include/* /usr/include "
 
