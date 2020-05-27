dda359bdba38d490bd496ed77a401002bbd768af
Tree Davies <tdavies@darkphysics.net>
2020-05-26 20:30:25 -0700

    Add 7.260-set-graphical-target.sh to enable system to boot with lightdm login prompt
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

---
+++ build-scripts/07-lxde/7.260-set-graphical-target.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /opt/physix/include.sh || exit 1
+
+if [ -L /etc/systemd/system/default.target ] ; then
+        rm /etc/systemd/system/default.target
+	chroot_check $? "rm link to default.target"
+fi
+
+ln -s  /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+chroot_check $? "link default.target"
+
