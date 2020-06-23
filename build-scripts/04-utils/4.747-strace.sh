959f8ef5a71b3a3174e0b960c4dd1ceedf2e1c43
Tree Davies <tdavies@darkphysics.net>
2020-06-23 12:37:49 -0700

    Add strace to 04-utilities
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

---
+++ build-scripts/04-utils/4.747-strace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /opt/admin/physix/include.sh || exit 1
+
+su physix -c './configure --prefix=/usr'
+chroot_check $? "configure"
+
+su physix -c "make -j$NPROC"
+chroot_check $? "make"
+
+make install
+chroot_check $? "make install"
+
