a9bafee3c3e5bc7cd94af63450e54c898925b64a
Tree Davies <tdavies@darkphysics.net>
2020-07-03 10:25:35 -0700

    Revert "Add TODO to remove snapshot from db when snapshots are deleted"
    This reverts commit 3ce96c298ba8bb88f182526dd1db672ff7b4e8bc.
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- pylib/ops.py
+++ pylib/ops.py
@@ -1088,8 +1088,6 @@ def do_delete_snapshot(options):
         validate(ret_tpl, "umount mntpoint")
         return FAILURE
 
-    #TODO: Remove snapshot from db
-
 
 def do_set_default_snapshot(options):
     """
