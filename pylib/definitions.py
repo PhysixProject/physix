7a026840af964bff121660a08c9306a1815daab2
Tree Davies <tdavies@darkphysics.net>
2020-08-04 12:39:37 -0700

    Populate definitions.py with path variable definitions
    modified:   definitions.py
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- pylib/definitions.py
+++ pylib/definitions.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python3
 
+BUILDROOT = "/mnt/physix"
 
+PHYSIX_DIR = '/opt/admin/physix'
+BUILDROOT_PHYSIX_DIR = '/mnt/physix/opt/admin/physix'
 
+DB_PATH = '/opt/admin/.DB.physix'
+BUILDROOT_DB_PATH = '/mnt/physix/opt/admin/.DB.physix'
+
+LOGS_DIR_PATH = '/opt/admin/logs.physix'
+BUILDROOT_lOGS_DIR_PATH = '/mnt/physix/opt/admin/logs.physix'
+
+SOURCES_DIR_PATH = '/opt/admin/sources.physix'
+BUILDROOT_SOURCES_DIR_PATH = '/mnt/physix/opt/admin/sources.physix'
+
+ADMIN_DIR_PATH = '/opt/admin'
+BUILDROOT_ADMIN_DIR_PATH = '/mnt/physix/opt/admin'
+
+BUILDSCRIPTS_DIR_PATH = '/opt/admin/physix/build-scripts'
+BUILDROOT_BUILDSCRIPTS_DIR_PATH = '/mnt/physix/opt/admin/physix/build-scripts'
+
+BUILDBOX_DIR_PATH= '/opt/admin/sources.physix/BUILDBOX'
+BUILDROOT_BUILDBOX_DIR_PATH= '/mnt/physix/opt/admin/sources.physix/BUILDBOX'
+
+PHYSIX_LOGFILE = '/opt/admin/logs.physix/physix-build.log'
+BUILDROOT_PHYSIX_LOGFILE = '/mnt/physix/opt/admin/logs.physix/physix-build.log'
+
+TMP_MNTPOINT_DIR = '/opt/admin/.tmp/mnt/'
+BUILDROOT_TMP_MNTPOINT_DIR = '/mnt/physix/opt/admin/.tmp/mnt/'
+
+BUILDLOCK_FILE = '/run/lock/buildbox.lock'
+BUILDROOT_BUILDLOCK_FILE = '/mnt/physix/run/lock/buildbox.lock'
 
