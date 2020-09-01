4599fd1d37d7874c3d4f986f51644a8de4482c7b
Tree Davies <tdavies@darkphysics.net>
2020-09-01 15:44:20 -0700

    Enable GCC test suite. Needs to be fixed to run test suite as nobody user.
    modified:   2.160-gcc.sh
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- build-scripts/02-base/2.160-gcc.sh
+++ build-scripts/02-base/2.160-gcc.sh
@@ -33,41 +33,37 @@ chroot_check $? "system-build : gcc : make "
 
 if [ "$CONF_RUN_ALL_TEST_SUITE"=="y" ] ; then
 	ulimit -s 32768
-	rm ../gcc/testsuite/g++.dg/pr83239.C
-	chown -Rv nobody .
+	make -k check
 
-    su nobody -s /bin/bash -c "PATH=$PATH make -k check"
-    ../contrib/test_summary
+	#rm ../gcc/testsuite/g++.dg/pr83239.C
+	#chown -Rv nobody .
+	#su nobody -s /bin/bash -c "PATH=$PATH make -k check"
+
+	../contrib/test_summary
 fi
 
 make install
 chroot_check $? "system-build : gcc  : make install"
 
-ln -sv ../usr/bin/cpp /lib
+ln -sv ../usr/bin/cpp /lib &&
 ln -sv gcc /usr/bin/cc
-install -v -dm755 /usr/lib/bfd-plugins
-ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/8.2.0/liblto_plugin.so \
-        /usr/lib/bfd-plugins
+chroot_check $? "ln cpp and cc"
 
-
+install -v -dm755 /usr/lib/bfd-plugins &&
+ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/8.2.0/liblto_plugin.so  /usr/lib/bfd-plugins
+chroot_check $? "install and link plugin"
 
 echo 'int main(){}' > dummy.c
-cc dummy.c -v -Wl,--verbose &> dummy.log
-readelf -l a.out | grep ': /lib'
-
+cc dummy.c -v -Wl,--verbose &> dummy.log && readelf -l a.out | grep ': /lib'
+chroot_check $? "cc dummy.c: Compiler looks broken"
 
 grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
-
 grep -B4 '^ /usr/include' dummy.log
-
 grep 'SEARCH.*/usr/lib' dummy.log |sed 's|; |\n|g'
-
 grep "/lib.*/libc.so.6 " dummy.log
-
 grep found dummy.log
 
 rm -v dummy.c a.out dummy.log
-
 mkdir -pv /usr/share/gdb/auto-load/usr/lib
 mv -v /usr/lib/*gdb.py /usr/share/gdb/auto-load/usr/lib
 
