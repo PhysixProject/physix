c62bc9fae09113e874ef767ea729190260056705
Tree Davies <tdavies@darkphysics.net>
2020-09-12 16:48:06 -0700

    During automated build of all recipes, Create snapshots after each recipe is built.
    modified:   BUILD_04-08.sh
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- BUILD_04-08.sh
+++ BUILD_04-08.sh
@@ -12,6 +12,13 @@ if [ `pwd` != '/opt/admin/physix' ] ; then
 	exit 1
 fi
 
+make-ca -g
+if [ $? -ne 0 ] ; then
+	echo "make-ca failure"
+	exit 1
+fi
+
+
 for RECIPE in  04-utilities.json 05-services.json 05-testing.json 05-xorg.json 06-lightdm.json 07-lxde.json 08-xapps.json ; do
 	./catalyst -p $RECIPE
 	if [ $? -ne 0 ] ; then
@@ -20,42 +27,49 @@ for RECIPE in  04-utilities.json 05-services.json 05-testing.json 05-xorg.json 0
 	fi
 done
 
+./catalyst -r SNAP_A
 ./catalyst -b 04-utilities.json
 if [ $? -ne 0 ] ; then
 	echo "Build Failed"
 	exit 1
 fi
 
+./catalyst -r SNAP_B
 ./catalyst -b 05-services.json
 if [ $? -ne 0 ] ; then
     echo "Build Failed"
     exit 1
 fi
 
+./catalyst -r SNAP_C
 ./catalyst -b 05-testing.json
 if [ $? -ne 0 ] ; then
     echo "Build Failed"
     exit 1
 fi
 
+./catalyst -r SNAP_D
 ./catalyst -b 05-xorg.json
 if [ $? -ne 0 ] ; then
     echo "Build Failed"
     exit 1
 fi
 
+./catalyst -r SNAP_E
 ./catalyst -b 06-lightdm.json
 if [ $? -ne 0 ] ; then
     echo "Build Failed"
     exit 1
 fi
 
+./catalyst -r SNAP_F
 ./catalyst -b 07-lxde.json
 if [ $? -ne 0 ] ; then
     echo "Build Failed"
     exit 1
 fi
 
+./catalyst -r SNAP_G
 ./catalyst -b 08-xapps.json
 if [ $? -ne 0 ] ; then
     echo "Build Failed"
