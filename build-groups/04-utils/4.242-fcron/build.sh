2527b4498792b3cdc9c2eb953bd4dcbe692cb476
Tree Davies <tdavies@darkphysics.net>
2020-09-23 13:58:06 -0700

    Add fcron to 04-utilities
    modified:   04-utilities.json
    new file:   build-groups/04-utils/4.242-fcron/build.sh
    new file:   build-groups/04-utils/4.242-fcron/usr.bin.runparts
    new file:   build-groups/04-utils/4.242-fcron/var.spool.fcron.systab.orig
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

---
+++ build-groups/04-utils/4.242-fcron/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+source ../include.sh || exit 1
+
+prep() {
+	return 0
+}
+
+config() {
+	./configure --prefix=/usr      \
+            --sysconfdir=/etc      \
+            --localstatedir=/var   \
+            --without-sendmail     \
+            --with-piddir=/run     \
+            --with-boot-install=no
+	chroot_check $? "configure"
+}
+
+build() {
+	make
+	chroot_check $? "make"
+}
+
+build_install() {
+	groupadd -g 22 fcron &&
+	useradd -d /dev/null -c "Fcron User" -g fcron -s /bin/false -u 22 fcron
+	chroot_check $? "Create Fcron User/group" NOEXIT
+
+	install -vdm754 /etc/cron.{hourly,daily,weekly,monthly}
+	chroot_check $? "mkdir /etc/cron.{hourly,daily,weekly,monthly}"
+
+	make install
+	chroot_check $? "make install"
+
+	install -v -m755 $PKG_DIR_PATH/usr.bin.runparts /usr/bin/run-parts
+	chroot_check $? "Install runparts"
+
+	install -v -m444 $PKG_DIR_PATH/var.spool.fcron.systab.orig /var/spool/fcron/systab.orig	
+	chroot_check $? "Install systab.orig"
+
+	systemctl enable fcron
+	chroot_check $? "systemctl enable fcron"
+
+	systemctl start fcron && fcrontab -z -u systab
+	chroot_check $? "Start fcron"
+}
+
+[ $1 == 'prep' ]   && prep   && exit $?
+[ $1 == 'config' ] && config && exit $?
+[ $1 == 'build' ]  && build  && exit $?
+[ $1 == 'build_install' ] && build_install && exit $?
+
