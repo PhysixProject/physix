62f279a882742949c90fd46770cbde10a89ccf76
Tree Davies <tdavies@darkphysics.net>
2021-01-26 17:04:42 -0800

    Upgrade systemd.
    Move util-linux up in order of recipe to build before systemd
    
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- build-groups/02-base/2.720-systemd/build.sh
+++ build-groups/02-base/2.720-systemd/build.sh
@@ -4,25 +4,23 @@
 source /opt/admin/physix/include.sh || exit 1
 cd $SOURCE_DIR/$1 || exit 1
 
-patch -Np1 -i ../systemd-243-consolidated_fixes-2.patch
-chroot_check $? "systemd : patch systemd-243-consolidated_fixes-2.patch"
-
 ln -sf /tools/bin/true /usr/bin/xsltproc
 chroot_check $? "systemd : ln -sf /tools/bin/true /usr/bin/xsltproc"
 
 
-for file in /tools/lib/lib{blkid,mount,uuid}.so*; do
-    ln -sf $file /usr/lib/
-done
+tar -xf ../systemd-man-pages-246.tar.xz
+chroot_check $? "systemd : tar -xf manpages"
 
-sed '177,$ d' -i src/resolve/meson.build &&
-sed -i 's/GROUP="render", //' rules/50-udev-default.rules.in
-chroot_check $? "systemd :"
+sed '177,$ d' -i src/resolve/meson.build
+chroot_check $? "error 1"
+
+sed -i 's/GROUP="render", //' rules.d/50-udev-default.rules.in
+chroot_check $? "error 2"
+
 
 mkdir -p build
 cd       build
 
-PKG_CONFIG_PATH="/usr/lib/pkgconfig:/tools/lib/pkgconfig" \
 LANG=en_US.UTF-8                   \
 meson --prefix=/usr                \
       --sysconfdir=/etc            \
@@ -43,6 +41,10 @@ meson --prefix=/usr                \
       -Dumount-path=/bin/umount    \
       -Db_lto=false                \
       -Drpmmacrosdir=no            \
+      -Dhomed=false                \
+      -Duserdb=false               \
+      -Dman=true                   \
+      -Ddocdir=/usr/share/doc/systemd-246 \
       ..
 chroot_check $? "systemd : configure"
 
@@ -66,15 +68,15 @@ chroot_check $? "systemd : disable systemd-time-wait-sync.service"
 
 rm -f /etc/sysctl.d/50-pid-max.conf
 chroot_check $? "systemd : rm -f /etc/sysctl.d/50-pid-max.conf"
-rm -fv /usr/lib/lib{blkid,uuid,mount}.so*
-chroot_check $? "systemd : rm -fv /usr/lib/lib{blkid,uuid,mount}.so*"
-rm -f /usr/lib/tmpfiles.d/systemd-nologin.conf
-chroot_check $? "systemd : rm -f /usr/lib/tmpfiles.d/systemd-nologin.conf"
+#rm -fv /usr/lib/lib{blkid,uuid,mount}.so*
+#chroot_check $? "systemd : rm -fv /usr/lib/lib{blkid,uuid,mount}.so*"
+#rm -f /usr/lib/tmpfiles.d/systemd-nologin.conf
+#chroot_check $? "systemd : rm -f /usr/lib/tmpfiles.d/systemd-nologin.conf"
 
 #By Default systemd install links default to graphical.target
 unlink /lib/systemd/system/default.target
 ln -s  /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
 
-sed -i 's/#Storage=.*/Storage=persistent/' /etc/systemd/journald.conf
-chroot_check $? "journald set to persist logs"
+#sed -i 's/#Storage=.*/Storage=persistent/' /etc/systemd/journald.conf
+#chroot_check $? "journald set to persist logs"
 
--- build-groups/04-utils/4.245-systemd/build.sh
+++ build-groups/04-utils/4.245-systemd/build.sh
@@ -4,12 +4,7 @@
 source ../include.sh || exit 1
 
 prep() {
-	patch -Np1 -i ../systemd-243-consolidated_fixes-2.patch
-	chroot_check $? "systemd : patch systemd-243-consolidated_fixes-2.patch"
-
-	sed '177,$ d' -i src/resolve/meson.build &&
-	sed -i 's/GROUP="render", //' rules/50-udev-default.rules.in
-	chroot_check $? "systemd :"
+	sed -i 's/GROUP="render", //' rules.d/50-udev-default.rules.in
 	mkdir -p build
 }
 
@@ -17,33 +12,31 @@ config() {
 
 	cd build
 
-	PKG_CONFIG_PATH="/usr/lib/pkgconfig" \
-	LANG=en_US.UTF-8                   \
-	meson --prefix=/usr                \
-	      --sysconfdir=/etc            \
-	      --localstatedir=/var         \
-	      -Dblkid=true                 \
-	      -Dbuildtype=release          \
-	      -Ddefault-dnssec=no          \
-	      -Dfirstboot=false            \
-	      -Dinstall-tests=false        \
-	      -Dkmod-path=/bin/kmod        \
-	      -Dldconfig=false             \
-	      -Dmount-path=/bin/mount      \
-	      -Drootprefix=                \
-	      -Drootlibdir=/lib            \
-	      -Dsplit-usr=true             \
-	      -Dsulogin-path=/sbin/sulogin \
-	      -Dsysusers=false             \
-	      -Dumount-path=/bin/umount    \
-	      -Db_lto=false                \
-	      -Drpmmacrosdir=no            \
-	      ..
+	meson --prefix=/usr             \
+		--sysconfdir=/etc           \
+		--localstatedir=/var        \
+		-Dblkid=true                \
+		-Dbuildtype=release         \
+		-Ddefault-dnssec=no         \
+		-Dfirstboot=false           \
+		-Dinstall-tests=false       \
+		-Dldconfig=false            \
+		-Dman=auto                  \
+		-Drootprefix=               \
+		-Drootlibdir=/lib           \
+		-Dsplit-usr=true            \
+		-Dsysusers=false            \
+		-Drpmmacrosdir=no           \
+		-Db_lto=false               \
+		-Dhomed=false               \
+		-Duserdb=false              \
+		-Ddocdir=/usr/share/doc/systemd-246 \
+		..
 	chroot_check $? "systemd : configure"
 }
 
 build() {
-	cd ./build  &&  LANG=en_US.UTF-8 ninja
+	cd ./build  && ninja
 	chroot_check $? "systemd : ninja"
 }
 
@@ -51,38 +44,15 @@ build_install() {
 
 	cd build
 
-        systemctl daemon-reload
-        chroot_check $? "Daemon Reload"
+    systemctl daemon-reload
+    chroot_check $? "Daemon Reload"
 
-        sync
-        sync
+    sync
+    sync
 
-	LANG=en_US.UTF-8 ninja install
-	chroot_check $? "systemd : ninja install"
-
-	systemctl daemon-reload
-	chroot_check $? "Daemon Reload"
-
-	systemd-machine-id-setup
-	chroot_check $? "systemd : systemd-machine-id-setup"
-
-	systemctl preset-all
-	chroot_check $? "systemd : systemctl preset-all"
-
-	systemctl disable systemd-time-wait-sync.service
-	chroot_check $? "systemd : disable systemd-time-wait-sync.service"
-
-	sed -i 's/#Storage=.*/Storage=persistent/' /etc/systemd/journald.conf
-	chroot_check $? "journald set to persist logs"
+	ninja install
+	chroot_check $? "ninja install"
 }
-
-#rm -f /etc/sysctl.d/50-pid-max.conf
-#chroot_check $? "systemd : rm -f /etc/sysctl.d/50-pid-max.conf"
-#rm -fv /usr/lib/lib{blkid,uuid,mount}.so*
-#chroot_check $? "systemd : rm -fv /usr/lib/lib{blkid,uuid,mount}.so*"
-#rm -f /usr/lib/tmpfiles.d/systemd-nologin.conf
-#chroot_check $? "systemd : rm -f /usr/lib/tmpfiles.d/systemd-nologin.conf"
-
 
 [ $1 == 'prep' ]   && prep   && exit $?
 [ $1 == 'config' ] && config && exit $?
