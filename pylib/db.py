d875f0d4ac9974625127c396c410e2686ab188c2
Tree Davies <tdavies@darkphysics.net>
2021-08-21 23:14:33 -0700

    Re-tab db.py
    Signed-off-by: Tree Davies <tdavies@darkphysics.net>

--- pylib/db.py
+++ pylib/db.py
@@ -21,150 +21,150 @@ from utils import *
 from definitions import *
 
 def get_db_connection(context):
-    """Return db connection object. Return None on error."""
-    db_path = ''
-    conn = None
+	"""Return db connection object. Return None on error."""
+	db_path = ''
+	conn = None
 
-    if context == "CHRT":
-        db_path = DB_PATH
-    elif context == "NON-CHRT":
-        db_path = BUILDROOT_DB_PATH
-    else:
-        error("get_db_connection(): Unknown context")
-        return None 
+	if context == "CHRT":
+		db_path = DB_PATH
+	elif context == "NON-CHRT":
+		db_path = BUILDROOT_DB_PATH
+	else:
+		error("get_db_connection(): Unknown context")
+		return None 
 
-    try:
-        conn = sqlite3.connect(db_path)
-    except Exception as e:
-        error(str(e))
-        return None
-    return conn
+	try:
+		conn = sqlite3.connect(db_path)
+	except Exception as e:
+		error(str(e))
+		return None
+	return conn
 
 
 def list_stack(conn):
-    """Read DB.physix stack table and output the entries. 
-       Return FAILUE/SUCCESS
+	"""Read DB.physix stack table and output the entries. 
+	   Return FAILUE/SUCCESS
 
-       Keyword arguments:
-       conn -- sqlite db object
-    """
+	   Keyword arguments:
+	   conn -- sqlite db object
+	"""
 
-    if os.path.exists('/mnt/physix/opt/admin/physix'):
-        context = 'NON-CHRT'
-    else:
-        context = 'CHRT'
+	if os.path.exists('/mnt/physix/opt/admin/physix'):
+		context = 'NON-CHRT'
+	else:
+		context = 'CHRT'
 
-    stack_name = get_name_current_stack(context)
+	stack_name = get_name_current_stack(context)
 
-    stack_lst = []
-    try:
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM "+stack_name)
-        stack_lst = cur.fetchall()
-    except Exception as e:
-        error(e)
-        return FAILURE
+	stack_lst = []
+	try:
+		cur = conn.cursor()
+		cur.execute("SELECT * FROM "+stack_name)
+		stack_lst = cur.fetchall()
+	except Exception as e:
+		error(e)
+		return FAILURE
 
-    info("stack::"+str(stack_name))
-    info("Stack Size: "+str(len(stack_lst)))
-    print('{:<6s} {:<20s} {:<10s} {:<10} {:<10s} {:<30s} {:<30s}'.format('key', 'DATE', 'OP', 'COMMIT', 'SNAP', 'PKG', 'SCRIPT' ))
-    for i in range(len(stack_lst)-1, -1, -1):
-        prim_key = str(stack_lst[i][0])
-        date     = str(stack_lst[i][1])
-        op       = str(stack_lst[i][2])
-        commit   = str(stack_lst[i][3])
-        snap     = str(stack_lst[i][4])
-        pkg      = str(stack_lst[i][5])
-        script   = str(stack_lst[i][6])
+	info("stack::"+str(stack_name))
+	info("Stack Size: "+str(len(stack_lst)))
+	print('{:<6s} {:<20s} {:<10s} {:<10} {:<10s} {:<30s} {:<30s}'.format('key', 'DATE', 'OP', 'COMMIT', 'SNAP', 'PKG', 'SCRIPT' ))
+	for i in range(len(stack_lst)-1, -1, -1):
+		prim_key = str(stack_lst[i][0])
+		date	 = str(stack_lst[i][1])
+		op		 = str(stack_lst[i][2])
+		commit	 = str(stack_lst[i][3])
+		snap	 = str(stack_lst[i][4])
+		pkg		 = str(stack_lst[i][5])
+		script	 = str(stack_lst[i][6])
 
-        listing = " ".join([prim_key, date, op, snap, pkg, script])
-        print('{:<6s} {:<20s} {:<10s} {:<10s} {:<10s} {:<30s} {:<30s}'.format(prim_key, date, op, commit, snap, pkg, script ))
-    return SUCCESS
+		listing = " ".join([prim_key, date, op, snap, pkg, script])
+		print('{:<6s} {:<20s} {:<10s} {:<10s} {:<10s} {:<30s} {:<30s}'.format(prim_key, date, op, commit, snap, pkg, script ))
+	return SUCCESS
 
 
 def init_db_tables():
-    """Create initial STACK_0 Table and initialize it.
-       Returns SUCCESS/FAILURE"""
+	"""Create initial STACK_0 Table and initialize it.
+	   Returns SUCCESS/FAILURE"""
 
-    if os.path.exists(BUILDROOT_DB_PATH):
-        return SUCCESS
+	if os.path.exists(BUILDROOT_DB_PATH):
+		return SUCCESS
 
-    conn = None
-    try:
-        conn = sqlite3.connect('/mnt/physix/opt/admin/.DB.physix')
-    except Exception as e:
-        error(str(e))
-        return FAILURE
+	conn = None
+	try:
+		conn = sqlite3.connect('/mnt/physix/opt/admin/.DB.physix')
+	except Exception as e:
+		error(str(e))
+		return FAILURE
 
-    sql_create_init_stack = """ CREATE TABLE IF NOT EXISTS STACK_0 (
-                                    ID integer PRIMARY KEY AUTOINCREMENT,
-                                    TIME text NOT NULL,
-                                    OP text NOT NULL,
-                                    COMMITID text,
-                                    SNAPID text,
-                                    PKG text,
-                                    SCRIPT text); """
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_create_init_stack, "")
-        conn.commit()
-    except Exception as e:
-        error(str(e))
-        return FAILURE
+	sql_create_init_stack = """ CREATE TABLE IF NOT EXISTS STACK_0 (
+									ID integer PRIMARY KEY AUTOINCREMENT,
+									TIME text NOT NULL,
+									OP text NOT NULL,
+									COMMITID text,
+									SNAPID text,
+									PKG text,
+									SCRIPT text); """
+	try:
+		cur = conn.cursor()
+		cur.execute(sql_create_init_stack, "")
+		conn.commit()
+	except Exception as e:
+		error(str(e))
+		return FAILURE
 
-    sql = ''' INSERT INTO STACK_0 (TIME,OP,COMMITID,SNAPID,PKG,SCRIPT) VALUES(?,?,?,?,?,?) '''
-    init_values = (date(), 'INIT', '', '', '','')
-    try:
-        cur = conn.cursor()
-        cur.execute(sql, init_values)
-        conn.commit()
-    except Exception as e:
-        error(str(e))
-        return FAILURE
+	sql = ''' INSERT INTO STACK_0 (TIME,OP,COMMITID,SNAPID,PKG,SCRIPT) VALUES(?,?,?,?,?,?) '''
+	init_values = (date(), 'INIT', '', '', '','')
+	try:
+		cur = conn.cursor()
+		cur.execute(sql, init_values)
+		conn.commit()
+	except Exception as e:
+		error(str(e))
+		return FAILURE
 
-    return SUCCESS
+	return SUCCESS
 
 
 def exec_sql(conn, sql, data):
-    """Executes an SQL query/command
+	"""Executes an SQL query/command
 
-       Keyword arguments:
-       conn -- sqlite db object
-       sql -- string: sql query/command
-       data -- string:data assiciated with the sql string
-    """
+	   Keyword arguments:
+	   conn -- sqlite db object
+	   sql -- string: sql query/command
+	   data -- string:data assiciated with the sql string
+	"""
 
-    try:
-        cur = conn.cursor()
-        cur.execute(sql, data)
-        conn.commit()
-    except Exception as e:
-        error(str(e))
-        return FAILURE
-    return SUCCESS
+	try:
+		cur = conn.cursor()
+		cur.execute(sql, data)
+		conn.commit()
+	except Exception as e:
+		error(str(e))
+		return FAILURE
+	return SUCCESS
 
 
 def write_db_stack_entry(context, cmt_id, operation, build_src, pkg_name):
-    '''
-    Write entry to physix.db
+	'''
+	Write entry to physix.db
 
-    Keyword arguments:
-    context --  STRING
-    cmt_id -- STRING
-    operation -- STRING
-    build_src  -- STRING
-    pkg_name -- STRING
-    '''
+	Keyword arguments:
+	context --	STRING
+	cmt_id -- STRING
+	operation -- STRING
+	build_src  -- STRING
+	pkg_name -- STRING
+	'''
 
-    db = get_db_connection(context)
-    if db:
-        stack_name = get_name_current_stack(context)
-        entry = (date(), operation, cmt_id, str(stack_name), build_src, str(pkg_name))
-        sql = "INSERT INTO "+ stack_name + " (TIME,OP,COMMITID,SNAPID,PKG,SCRIPT) VALUES(?,?,?,?,?,?) "
-        info("Stack: " + stack_name)
-        if exec_sql(db, sql, entry):
-            error("DB: Failed to insert entry")
-            return FAILURE
-        db.close()
-    return SUCCESS
+	db = get_db_connection(context)
+	if db:
+		stack_name = get_name_current_stack(context)
+		entry = (date(), operation, cmt_id, str(stack_name), build_src, str(pkg_name))
+		sql = "INSERT INTO "+ stack_name + " (TIME,OP,COMMITID,SNAPID,PKG,SCRIPT) VALUES(?,?,?,?,?,?) "
+		info("Stack: " + stack_name)
+		if exec_sql(db, sql, entry):
+			error("DB: Failed to insert entry")
+			return FAILURE
+		db.close()
+	return SUCCESS
 
